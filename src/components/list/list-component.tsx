ef7b977d7c0947d9287597fd2940bd483df9c027
Eimar Ramos <eimar.ramos@lodisna.com>
2024-05-24 08:42:06 +0200

    feat: OnChildClick

--- src/components/list/list-component.tsx
+++ src/components/list/list-component.tsx
@@ -1,11 +1,18 @@
 import { createElement } from "react";
 import { ListItemComponent } from "./list-item/list-item-component";
 
+
 export const ListComponent = () => {
+
+  const childClick = (content: string) => {
+    console.log("Hijo hace click", content)
+  };
+  
+
   const items: JSX.Element[] = [
-    <ListItemComponent content="Item1" isDone={false}></ListItemComponent>,
-    <ListItemComponent content="Item2" isDone={true}></ListItemComponent>,
-    <ListItemComponent content="Item3" isDone={false}></ListItemComponent>,
+    <ListItemComponent content="Item1" isDone={false} onChildClick={childClick}></ListItemComponent>,
+    <ListItemComponent content="Item2" isDone={true} onChildClick={childClick}></ListItemComponent>,
+    <ListItemComponent content="Item3" isDone={false} onChildClick={childClick}></ListItemComponent>,
   ];
 
   return createElement("ul", { id: "list" }, ...items);
--- src/components/list/list-item/list-item-component.tsx
+++ src/components/list/list-item/list-item-component.tsx
@@ -1,18 +1,22 @@
 import React, { createElement } from "react";
 
+
+
 type props = {
   isDone: boolean;
   content: string;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  onChildClick: Function;
 };
 
-export const ListItemComponent: React.FC<props> = ({ isDone, content }) => {
+export const ListItemComponent: React.FC<props> = ({ content, onChildClick}) => {
   return createElement(
     "li",
     {
-      isDone: isDone,
+      isDone: false,
       className: "list-item",
-      onClick: () => {
-        console.log("Reacciono!", content);
+      onClick: (e: Event) => {
+        onChildClick(content, e);
       },
     },
     content
